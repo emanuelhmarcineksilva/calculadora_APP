c4b010148f99c6e5878b776cbd4213bed8574a74
Emanuel <105359101+emanuelhmarcineksilva@users.noreply.github.com>
2024-11-22 15:20:25 -0300

    Update min.py

--- min.py
+++ min.py
@@ -33,6 +33,10 @@ frame_corpo.grid(row=1, column=0)
 # Variavel todos valores.
 todos_valores = ''
 
+# Criando label.
+valor_texto = StringVar()
+
+
 # Criando um função
 # O 'event' recebe um valor que pode ser posto nas ativações dessa função.
 def entrar_valores(event):
@@ -48,8 +52,22 @@ def entrar_valores(event):
     valor_texto.set(todos_valores)
 
 
+# Função para calcular.
+def calcular():
+    global todos_valores
+    resultado = eval(todos_valores)
+    valor_texto.set(str(resultado))
+    todos_valores = str(resultado)
 
-# Criando label.
+
+# Fujnção limpar tela.
+def limpar_tela():
+    # Foi colocado global apra que também todas as váriaveis todos_valores tenham o memso resultado.
+    global todos_valores
+    todos_valores = ''
+    valor_texto.set('')
+
+
 # Aqui será criada a caixa de resposta.
 
 # O código 'padx=' é um padding (espaçamento interno) no eixo da abissisa (x).
@@ -60,7 +78,6 @@ def entrar_valores(event):
 # O código 'fg=' define a cor do texto.
 # O código 'StringVar()' cria uma varivel de valor string, que ira receber um valor variavel.
 # O código 'textvariable=' permite que os valores dele sejam alterados dinamicamente.
-valor_texto = StringVar()
 app_label = Label(frame_janela, textvariable=valor_texto, width=14, height=1, padx=5, pady=7, font=('Ivy 20'), relief=FLAT, anchor="e", justify=RIGHT, bg=cor3, fg=cor2)
 app_label.place(x=0, y=0)
 
@@ -71,7 +88,7 @@ app_label.place(x=0, y=0)
 # O código 'font=()' define a fonte que será a 'Ivy', o tamanho que será 13q, e podera definir se estará em negrito com 'bold'.
 # O código 'relief=' define o estilo que será um que da a impreção que o botão sobe um pouco para frente, o RAISED, se você escrever sem as aspas deixe o nome com todas as letras maiúsculas, se for com as aspas deixe com as letras minúsculas.
 # O código 'overrelief' faz com que o botão execute uma ação quando o mouse passar por cima do botão, que no caso é a RIDGE.
-b_1 = Button(frame_corpo, height=2, width=11, text="C", bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_1 = Button(frame_corpo, height=2, width=11, command= limpar_tela, text="C", bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 # O código 'place()' indica onde estara a posição na tela indicada.
 b_1.place(x=0, y=0)
 # O código 'command' executa o código quando uma ação é feita, exemplo, quando apertão o botão.
@@ -113,7 +130,7 @@ b_16 = Button(frame_corpo, height=2, width=11, command= lambda: entrar_valores('
 b_16.place(x=0, y=208)
 b_17 = Button(frame_corpo, height=2, width=5, command= lambda: entrar_valores('.'), text=".", bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_17.place(x=120, y=208)
-b_18 = Button(frame_corpo, height=2, width=5, command= lambda: entrar_valores('='), text="=", bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_18 = Button(frame_corpo, height=2, width=5, command= calcular, text="=", bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_18.place(x=180, y=208)
 
 # Configurando os calculos
